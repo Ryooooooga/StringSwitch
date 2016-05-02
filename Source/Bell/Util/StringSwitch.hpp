a51384548649c3ab7b85b5e8b26a8f5d57443075
Ryoga <ryoga_314@yahoo.co.jp>
2016-05-03 08:01:16 +0900

    :new: First Commit

---
+++ Source/Bell/Util/StringSwitch.hpp
@@ -0,0 +1,87 @@
+﻿//=====================================================================
+// Copyright (c) 2015-2016 Ryooooooga.
+// https://github.com/Ryooooooga
+//
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+//=====================================================================
+
+#pragma once
+
+#include <boost/utility/string_ref.hpp>
+
+namespace Bell {
+
+	/**
+	 * @brief      BasicStringSwitch
+	 *
+	 * @tparam     CharType  文字型
+	 * @tparam     Traits    トレイト
+	 */
+	template <typename ValueType, typename CharType, typename Traits = std::char_traits<CharType>>
+	class BasicStringSwitch
+	{
+		using StringRefType = boost::basic_string_ref<CharType, Traits>;
+
+		StringRefType		key_;
+		const ValueType*	value_;
+
+	public:
+		/**
+		 * @brief      BasicStringRef
+		 *
+		 * @param[in]  s
+		 */
+		explicit BasicStringSwitch(StringRefType s) noexcept
+			: key_(s), value_(nullptr) {}
+
+
+		/**
+		 * @brief      case
+		 *
+		 * @param[in]  s
+		 * @param[in]  value
+		 *
+		 * @return     *this
+		 */
+		BasicStringSwitch& _case(StringRefType s, const ValueType& value) noexcept
+		{
+			if (!value_ && key_ == s)
+			{
+				value_ = &value;
+			}
+
+			return *this;
+		}
+
+		/**
+		 * @brief      default
+		 *
+		 * @param[in]  value
+		 *
+		 * @return
+		 */
+		const ValueType& _default(const ValueType& value) const noexcept
+		{
+			return value_ ? *value_ : value;
+		}
+
+		/**
+		 * @brief
+		 *
+		 * @return
+		 */
+		const ValueType& value() const noexcept
+		{
+			assert(value_);
+			return *value_;
+		}
+	};
+
+	template <typename ValueType> using StringSwitch	= BasicStringSwitch<ValueType, char>;
+	template <typename ValueType> using WStringSwitch	= BasicStringSwitch<ValueType, wchar_t>;
+	template <typename ValueType> using U16StringSwitch	= BasicStringSwitch<ValueType, char16_t>;
+	template <typename ValueType> using U32StringSwitch	= BasicStringSwitch<ValueType, char32_t>;
+
+}	//	namespace Bell
---
+++ Test/Bell/Util/Test_StringSwitch.cpp
@@ -0,0 +1,139 @@
+﻿//=====================================================================
+// Copyright (c) 2015-2016 Ryooooooga.
+// https://github.com/Ryooooooga
+//
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+//=====================================================================
+
+#include <gtest/gtest.h>
+#include "../../../Source/Bell/Util/StringSwitch.hpp"
+
+TEST(Bell_StringSwitch, StringSwitch)
+{
+	using namespace Bell;
+
+	EXPECT_EQ(
+		2,
+		StringSwitch<int>(u8"b")
+			._case(u8"a", 1)
+			._case(u8"b", 2)
+			._case(u8"c", 3)
+			.value()
+	);
+
+	EXPECT_EQ(
+		2,
+		StringSwitch<int>(u8"b")
+			._case(u8"a", 1)
+			._case(u8"b", 2)
+			._case(u8"c", 3)
+			._default(-1)
+	);
+
+	EXPECT_EQ(
+		-1,
+		StringSwitch<int>(u8"z")
+			._case(u8"a", 1)
+			._case(u8"b", 2)
+			._case(u8"c", 3)
+			._default(-1)
+	);
+}
+
+TEST(Bell_StringSwitch, WStringSwitch)
+{
+	using namespace Bell;
+
+	EXPECT_EQ(
+		2,
+		WStringSwitch<int>(L"b")
+			._case(L"a", 1)
+			._case(L"b", 2)
+			._case(L"c", 3)
+			.value()
+	);
+
+	EXPECT_EQ(
+		2,
+		WStringSwitch<int>(L"b")
+			._case(L"a", 1)
+			._case(L"b", 2)
+			._case(L"c", 3)
+			._default(-1)
+	);
+
+	EXPECT_EQ(
+		-1,
+		WStringSwitch<int>(L"z")
+			._case(L"a", 1)
+			._case(L"b", 2)
+			._case(L"c", 3)
+			._default(-1)
+	);
+}
+
+TEST(Bell_StringSwitch, U16StringSwitch)
+{
+	using namespace Bell;
+
+	EXPECT_EQ(
+		2,
+		U16StringSwitch<int>(u"b")
+			._case(u"a", 1)
+			._case(u"b", 2)
+			._case(u"c", 3)
+			.value()
+	);
+
+	EXPECT_EQ(
+		2,
+		U16StringSwitch<int>(u"b")
+			._case(u"a", 1)
+			._case(u"b", 2)
+			._case(u"c", 3)
+			._default(-1)
+	);
+
+	EXPECT_EQ(
+		-1,
+		U16StringSwitch<int>(u"z")
+			._case(u"a", 1)
+			._case(u"b", 2)
+			._case(u"c", 3)
+			._default(-1)
+	);
+}
+
+TEST(Bell_StringSwitch, U32StringSwitch)
+{
+	using namespace Bell;
+
+	EXPECT_EQ(
+		2,
+		U32StringSwitch<int>(U"b")
+			._case(U"a", 1)
+			._case(U"b", 2)
+			._case(U"c", 3)
+			.value()
+	);
+
+	EXPECT_EQ(
+		2,
+		U32StringSwitch<int>(U"b")
+			._case(U"a", 1)
+			._case(U"b", 2)
+			._case(U"c", 3)
+			._default(-1)
+	);
+
+	EXPECT_EQ(
+		-1,
+		U32StringSwitch<int>(U"z")
+			._case(U"a", 1)
+			._case(U"b", 2)
+			._case(U"c", 3)
+			._default(-1)
+	);
+}
