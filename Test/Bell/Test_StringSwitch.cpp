e75bc49a5b4ff1c393e7b0a75f9f712d5e945fab
Ryoga <ryoga_314@yahoo.co.jp>
2016-05-12 22:36:44 +0900

    :name_badge: テストファイルのディレクトリを修正

--- Test/Bell/Test_StringSwitch.cpp
+++ Test/Bell/Test_StringSwitch.cpp
@@ -8,7 +8,7 @@
 //=====================================================================
 
 #include <gtest/gtest.h>
-#include "../../../Bell/StringSwitch.hpp"
+#include "../../Bell/StringSwitch.hpp"
 
 TEST(Bell_StringSwitch, StringSwitch)
 {
